c8da480d5ac7e71942c66b08023ea4e3dd39f6bf
James <jimmiexd@gmail.com>
2023-09-03 21:03:08 -0400

    Complete ReWrite

--- Bugger.cs
+++ Bugger.cs
@@ -1,96 +1,158 @@
 ﻿using System;
-using System.Diagnostics;
-using Octokit;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 
-namespace GitBugger {
+namespace SquirrelUtils.GitBugger {
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    [SuppressMessage("ReSharper", "SwitchStatementHandlesSomeKnownEnumValuesWithDefault")]
     public class Bugger {
-        private static GitHubClient _client;
-        private static readonly Lazy<Bugger> GitBugger = new Lazy<Bugger>(() => new Bugger());
-        /// <summary>
-        /// The Bugger Instance
-        /// </summary>
-        public static Bugger Instance => GitBugger.Value;
+        private const string GithubUrl = "https://api.github.com/repos/";
+        private const int GithubWaitForbidden = 60;
+        public string? Token;
 
-        private Bugger() { }
-        /// <summary>
-        /// Creates a new client using the given header and token
-        /// </summary>
-        /// <example>
-        /// The token doesn't work anymore don't try
-        /// Bugger.NewClient("MyApp", new Credentials("f25dccab1326d0b6a78bba91d1a501b4c7456cd6"));
-        /// </example>
-        /// <param name="header">The Product Header</param>
-        /// <param name="token">Your developer token</param>
-        public static void NewClient(string header, Credentials token) {
-            try {
-                _client = new GitHubClient(new ProductHeaderValue(header)) {Credentials = token};
+        private HttpClient? _client;
+
+        private HttpClient GetClient() {
+            if (_client != null) {
+                return _client;
             }
-            catch (Exception ex) {
-                Debug.Write(ex);
+            _client = new HttpClient();
+            _client.DefaultRequestHeaders.Add("Connection", "close");
+            _client.DefaultRequestHeaders.Add("User-Agent", "GitBugger-Client");
+            _client.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
+            _client.DefaultRequestHeaders.Add("Authorization", $"token {Token}");
+            return _client;
+        }
+
+        /// <summary>
+        /// Submits and issue to give repo
+        /// </summary>
+        /// <param name="owner">The repo owner</param>
+        /// <param name="repo">The repo</param>
+        /// <param name="issue">The GithubIssue used</param>
+        public async Task SubmitIssue(string owner, string repo, GithubIssue issue) {
+            while (true) {
+                HttpClient httpClient = GetClient();
+                HttpResponseMessage response = await httpClient.PostAsync($"{GithubUrl}{owner}/{repo}/issues", JsonContent.Create(issue));
+                switch (response.StatusCode) {
+                    case HttpStatusCode.Created:
+                        Console.WriteLine("Done");
+                        break;
+                    case HttpStatusCode.Forbidden:
+                        Console.Write("Too many requests. Github is mad. Will retry in 60 seconds...");
+                        await Task.Delay(GithubWaitForbidden);
+                        continue;
+                    default: {
+                        string errorMessage = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine(errorMessage);
+                        break;
+                    }
+                }
+
+                break;
             }
         }
+
         /// <summary>
-        /// Creates an issue with the label "bug"
+        /// The record used in creating a new Issue
         /// </summary>
-        /// <example>
-        /// Bugger.CreateBug("MrSquirrely", "UpdateJsons", Bugger.Issue("test1","test1"));
-        /// </example>
-        /// <param name="repoOwner">This is the owner of the repo</param>
-        /// <param name="repo">This is the repo name</param>
-        /// <param name="issue">Use Bugger.Issue to fill this out</param>
-        public static async void CreateBug(string repoOwner, string repo, NewIssue issue) {
-            issue.Labels.Add("bug");
-            await _client.Issue.Create(repoOwner, repo, issue);
-        }
-        /// <summary>
-        /// Creates an issue with the label "enhancement"
-        /// </summary>
-        /// <example>
-        /// Bugger.CreateFeature("MrSquirrely", "UpdateJsons", Bugger.Issue("test2", "test2"));
-        /// </example>
-        /// <param name="repoOwner">This is the owner of the repo</param>
-        /// <param name="repo">This is the repo name</param>
-        /// <param name="issue">Use Bugger.Issue to fill this out</param>
-        public static async void CreateFeature(string repoOwner, string repo, NewIssue issue) {
-            issue.Labels.Add("enhancement");
-            await _client.Issue.Create(repoOwner, repo, issue);
-        }
-        /// <summary>
-        /// Creates an issue with the given label
-        /// </summary>
-        /// <example>
-        /// Bugger.CreateIssue("MrSquirrely", "UpdateJsons", Bugger.Issue("test3", "test3"), "documentation");
-        /// </example>
-        /// <param name="repoOwner">This is the owner of the repo</param>
-        /// <param name="repo">This is the repo name</param>
-        /// <param name="issue">Use Bugger.Issue to fill this out</param>
-        /// <param name="label">The label you want to use, The label needs to exist or it wont work</param>
-        public static async void CreateIssue(string repoOwner, string repo, NewIssue issue, string label) {
-            issue.Labels.Add(label);
-            await _client.Issue.Create(repoOwner, repo, issue);
-        }
-        /// <summary>
-        /// Creates an issue with the given labels
-        /// </summary>
-        /// <example>
-        /// Bugger.CreateIssue("MrSquirrely", "UpdateJsons", Bugger.Issue("test3", "test3"), new []{"duplicate","good first issue", "invalid"});
-        /// </example>
-        /// <param name="repoOwner">This is the owner of the repo</param>
-        /// <param name="repo">This is the repo name</param>
-        /// <param name="issue">Use Bugger.Issue to fill this out</param>
-        /// <param name="labels">The labels you want to use, The labels needs to exist or it wont work</param>
-        public static async void CreateIssue(string repoOwner, string repo, NewIssue issue, string[] labels) {
-            foreach (string label in labels) {
-                issue.Labels.Add(label);
+        /// <param name="Title">The title of the issue</param>
+        /// <param name="Body">The body of the issue</param>
+        public record GithubIssue(string Title, string Body) {
+            public List<string> Labels { get; set; } = new();
+            public string? Milestone { get; set; }
+            public List<string> Assignees { get; set; } = new();
+
+            /// <summary>
+            /// Used to add a label to an issue
+            /// The label needs to exist in the Github repo or Github will just ignore the label
+            /// </summary>
+            /// <param name="label">The label to add</param>
+            /// <returns>GithubIssue</returns>
+            public GithubIssue AddLabel(string label) {
+                Labels.Add(label);
+                return this;
             }
-            await _client.Issue.Create(repoOwner, repo, issue);
+
+            /// <summary>
+            /// Used to add labels to an issue
+            /// The labels need to exist in the Github repo or Github will just ignore the labels
+            /// </summary>
+            /// <param name="labels">The labels to add</param>
+            /// <returns>GithubIssue</returns>
+            public GithubIssue AddLabels(string[] labels) {
+                Labels.AddRange(labels);
+                return this;
+            }
+
+            /// <summary>
+            /// Used to add labels to an issue
+            /// The labels need to exist in the Github repo or Github will just ignore the labels
+            /// </summary>
+            /// <param name="labels">The labels to add</param>
+            /// <returns>GithubIssue</returns>
+            public GithubIssue AddLabels(List<string> labels) {
+                Labels.AddRange(labels);
+                return this;
+            }
+
+            /// <summary>
+            /// Used to add a milestone label to an issue
+            /// The Milestone needs to exist in the repo or Github will just ignore this
+            /// </summary>
+            /// <param name="milestone">The milestone to add</param>
+            /// <returns>GithubIssue</returns>
+            public GithubIssue AddMilestone(string? milestone) {
+                Milestone = milestone;
+                return this;
+            }
+
+            /// <summary>
+            /// Used to add a milestone label to an issue
+            /// The Milestone needs to exist in the repo or Github will just ignore this
+            /// </summary>
+            /// <param name="milestone">the milestone to add</param>
+            /// <returns>GithubIssue</returns>
+            public GithubIssue AddMilestone(int milestone) {
+                Milestone = milestone.ToString();
+                return this;
+            }
+
+            /// <summary>
+            /// Used to add an assignee to an issue
+            /// </summary>
+            /// <param name="assignee">The Github user to add</param>
+            /// <returns>GithubIssue</returns>
+            public GithubIssue AddAssignee(string assignee) {
+                Assignees.Add(assignee);
+                return this;
+            }
+
+            /// <summary>
+            /// Used to add assignees to an issue
+            /// </summary>
+            /// <param name="assignees">The Github users to add</param>
+            /// <returns>GithubIssue</returns>
+            public GithubIssue AddAssignees(List<string> assignees) {
+                Assignees.AddRange(assignees);
+                return this;
+            }
+
+            /// <summary>
+            /// Used to add assignees to an issue
+            /// </summary>
+            /// <param name="assignees">The Github users to add</param>
+            /// <returns>GithubIssue</returns>
+            public GithubIssue AddAssignees(string[] assignees) {
+                Assignees.AddRange(assignees);
+                return this;
+            }
+
         }
-        /// <summary>
-        /// Returns "NewIssue" for ease
-        /// </summary>
-        /// <param name="title">The issue title</param>
-        /// <param name="body">The body of the issue</param>
-        /// <returns></returns>
-        public static NewIssue Issue(string title, string body) => new NewIssue(title) { Body = body };
+
     }
 }
